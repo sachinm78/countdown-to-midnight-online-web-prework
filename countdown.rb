a4c2d818b2903061fdeb14839a42c70959fc3fd5
Sachin Mahajan <sachinm78@gmail.com>
2019-03-10 17:59:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
